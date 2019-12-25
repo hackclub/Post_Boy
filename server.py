be4bd4d51d24c711aa7ccc688ef2a25956990edc
Frank Hui <huifrank@live.com>
2019-12-24 20:26:36 -0800

    Fix slack api errors

--- server.py
+++ server.py
@@ -24,35 +24,36 @@ def index():
     with open('index.html', 'r') as myfile:
         return myfile.read()
 
+
 @app.route('/img/<path>')
 def send_assets(path):
     return send_from_directory('img', path)
+
 
 @app.route('/<path>')
 def send_style(path):
     return send_from_directory('static', path)
 
+
 @app.route('/user', methods=["GET", "POST"])
 def user():
-    # # Retrieve the auth code from the request params
-    # auth_code = request.args['code']
-    #
-    # # An empty string is a valid token for this request
-    # client = slack.WebClient(token="")
-    #
-    # # Request the auth tokens from Slack
-    # response = client.oauth_v2_access(
-    #     client_id=client_id,
-    #     client_secret=client_secret,
-    #     code=auth_code
-    # )
-    #
-    # user_client = slack.WebClient(token=response['access_token'])
-    # info = user_client.users_info()
-    info = {'user':{'id':'UJ9864R4N', 'real_name':'Frank Hui'}}
+    # Retrieve the auth code from the request params
+    auth_code = request.args['code']
+
+    # An empty string is a valid token for this request
+    client = slack.WebClient(token="")
+    print(client_id)
+    # Request the auth tokens from Slack
+    response = client.oauth_access(client_id=client_id, client_secret=client_secret, code=auth_code)
+
+    user_client = slack.WebClient(token=response['access_token'])
+    info = user_client.users_identity()
+    print(info)
+    # info = {'user':{'id':'UJ9864R4N', 'real_name':'Frank Hui'}}
     user_slack_id = info['user']['id']
-    user_name = info['user']['real_name']
+    user_name = info['user']['name']
     packages = []
+    num_of_packages = 0
     isNodeMaster = False
     with open('database.json') as json_file:
         database = json.load(json_file)
@@ -63,16 +64,18 @@ def user():
             elif 'type' not in package.keys():
                 if package['date_shipped'] is not "NS":
                     package['date_shipped'] = datetime.datetime.utcfromtimestamp(1347517370).strftime(
-                    '%Y-%m-%d %H:%M:%S') + " UTC"
+                        '%Y-%m-%d %H:%M:%S') + " UTC"
                 if package['date_arrived'] is not "NA":
                     package['date_shipped'] = datetime.datetime.utcfromtimestamp(1347517370).strftime(
-                    '%Y-%m-%d %H:%M:%S') + " UTC"
+                        '%Y-%m-%d %H:%M:%S') + " UTC"
+                if package['status'] is not 'A':
+                    num_of_packages += 1
                 packages.append(package)
 
     else:
         return "User not in database."
 
     if len(packages) == 0:
-        return "No packages found."
-    num_of_packages = len(packages)
+        return render_template("statuslist.html", packages=packages, name=user_name, len=num_of_packages)
+
     return render_template("statuslist.html", packages=packages, name=user_name, len=num_of_packages)
